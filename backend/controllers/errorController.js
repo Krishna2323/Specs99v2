49a31cbf0ea3c5199a36321fe0769968ba82f1ae
Krishna2323 <85894871+Krishna2323@users.noreply.github.com>
2022-08-09 11:55:39 +0530

    09-08-2022 Stripe Webhook Chechout Test-4.

--- backend/controllers/errorController.js
+++ backend/controllers/errorController.js
@@ -46,6 +46,7 @@ const sendErrorDev = (err, req, res) => {
 
 const sendErrorProd = (err, req, res) => {
   // A) API
+  console.error('ERROR 💥', err);
   if (req.originalUrl.startsWith('/api')) {
     // A) Operational, trusted error: send message to client
     if (err.isOperational) {
@@ -56,7 +57,6 @@ const sendErrorProd = (err, req, res) => {
     }
     // B) Programming or other unknown error: don't leak error details
     // 1) Log error
-    console.error('ERROR 💥', err);
     // 2) Send generic message
     return res.status(500).json({
       status: 'error',
