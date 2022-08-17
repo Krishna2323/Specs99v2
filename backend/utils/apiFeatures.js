4a0e88b8167b438419973d7ed733cd9673c60ead
Krishna2323 <85894871+Krishna2323@users.noreply.github.com>
2022-08-17 23:23:54 +0530

    17-08-2022 Products Page Bug Fixed-1

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -51,7 +51,7 @@ class ApiFeature {
 
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(queryStr);
+    // console.log(queryStr);
     this.query = this.query.find(JSON.parse(queryStr));
 
     return this;
